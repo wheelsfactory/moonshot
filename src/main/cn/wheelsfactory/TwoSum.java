68ad675f08c29797c23304b0c8d689dcc65f3929
吕一凡 <lvyifan@cnipr.com>
2018-09-29 09:24:07 +0800

    fix #1

--- src/main/cn/wheelsfactory/TwoSum.java
+++ src/main/cn/wheelsfactory/TwoSum.java
@@ -2,6 +2,20 @@ package cn.wheelsfactory;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        return null;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        int temp = 0;
+        for(int i=0;i<nums.length;i++){
+            temp = target - nums[i];
+            if(map.containsKey(temp) && map.get(temp) != i){
+                result[0] = i;
+                result[1] = map.get(temp);
+                break;
+            }
+        }
+		return result;
     }
 }
